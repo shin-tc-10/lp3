ba1d1382346f9e2bb56afc2a6e1129d331d8b9f4
shin <shin.nkt.09@gmail.com>
2025-05-10 20:28:58 +0900

    スマホレイアウト作成

---
+++ assets/script/main.js
@@ -0,0 +1,46 @@
+$(() => {
+    const $items = $('.reviews__item');
+  
+    $('.reviews__items').slick({
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '40px',
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        customPaging: function (slider, i) {
+          return '<span class="reviews__dot"></span>';
+        },
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true,
+              centerPadding: '40px',
+            }
+          },
+          {
+            breakpoint: 9999,
+            settings: {
+              slidesToShow: 3,
+              centerMode: false,
+            }
+          }
+        ]
+      });
+      
+      
+  
+    // スライド切り替え時にis-activeをつける
+    $('.reviews__items').on('afterChange', function (event, slick, currentSlide) {
+      $items.removeClass('is-active');
+      $items.eq(currentSlide).addClass('is-active');
+    });
+  
+    // 初期is-active設定
+    $items.removeClass('is-active');
+    $items.eq(0).addClass('is-active');
+  });
+  
